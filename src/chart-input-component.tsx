ad9f77363e9190cb920252579d326c73b82bd930
maridius98 <maridius98@yahoo.com>
2024-02-02 00:16:31 +0100

    applying pause/interval to all functions

--- src/chart-input-component.tsx
+++ src/chart-input-component.tsx
@@ -5,200 +5,194 @@ import { Observable, take, tap } from 'rxjs';
 import { createRealTimeLineChart, point } from './diagram';
 
 export const ChartWithInputs = () => {
-const componentRef = useRef(null);
-const chartRef = useRef(null);
-const signalRef = useRef<Signal>(null);
-const signalsRef = useRef<Signal[]>(null);
-const subscriptionRef = useRef(null);
+	const componentRef = useRef(null);
+	const chartRef = useRef(null);
+	const signalRef = useRef<Signal>(null);
+	const signalsRef = useRef<Signal[]>(null);
+	const subscriptionRef = useRef(null);
 
-const [formula, setFormula] = useState('sin(t^2)');
-const [formulas, setFormulas] = useState(['t/10', 'cos(t)']);
-const [intervalValue, setIntervalValue] = useState(1);
-const [isPaused, setIsPaused] = useState(true);
-const [filterValue, setFilterValue] = useState(-0.4);
-const [samplesValue, setLengthValue] = useState(3000);
-const [mergeFunctions, setMergeFunctions] = useState(false);
+	const [formula, setFormula] = useState('sin(t^2)');
+	const [formulas, setFormulas] = useState(['t/10', 'cos(t)']);
+	const [intervalValue, setIntervalValue] = useState(1);
+	const [isPaused, setIsPaused] = useState(true);
+	const [filterValue, setFilterValue] = useState(-0.4);
+	const [samplesValue, setLengthValue] = useState(3000);
+	const [mergeFunctions, setMergeFunctions] = useState(false);
 
-const handleFormula1Change = (event: any) => {
-	event.preventDefault();
-	setFormula(event.target.value);
-}; 			
+	const handleFormula1Change = (event: any) => {
+		event.preventDefault();
+		setFormula(event.target.value);
+	}; 			
 
-const handleFormulaChange = (index: number) => (event: any) => {
-	event.preventDefault();
-	const newFormulas = [...formulas];
-	newFormulas[index] = event.target.value;
-	setFormulas(newFormulas);
-};
+	const handleFormulaChange = (index: number) => (event: any) => {
+		event.preventDefault();
+		const newFormulas = [...formulas];
+		newFormulas[index] = event.target.value;
+		setFormulas(newFormulas);
+	};
 
-const handleIntervalChange = (event: any) => {
-	setIntervalValue(Number(event.target.value));
-};
+	const handleIntervalChange = (event: any) => {
+		setIntervalValue(Number(event.target.value));
+	};
 
-const handlePauseToggle = () => {
-	setIsPaused((prevIsPaused) => !prevIsPaused);
-};
+	const handlePauseToggle = () => {
+		setIsPaused((prevIsPaused) => !prevIsPaused);
+	};
 
-const handleFilterChange = (event: any) => {
-	setFilterValue(Number(event.target.value));
-};
+	const handleFilterChange = (event: any) => {
+		setFilterValue(Number(event.target.value));
+	};
 
-const handleSamplesChange = (event: any) => {
-	setLengthValue(Number(event.target.value));
-};
+	const handleSamplesChange = (event: any) => {
+		setLengthValue(Number(event.target.value));
+	};
 
-const handleMergeFunctionsToggle = () => {
-	setMergeFunctions((prevMergeFunctions) => !prevMergeFunctions);
-};
+	const handleMergeFunctionsToggle = () => {
+		setMergeFunctions((prevMergeFunctions) => !prevMergeFunctions);
+	};
 
-const renderChart = (signal: Observable<point>, index: number, chart: any) => {
-	subscriptionRef.current = signal.subscribe(point => {
-	chart.updateChart(point, index+1)
-	})
-}
-
-const appendOperators = () => {
-	if (mergeFunctions) {
-	formulaParser.evaluate(`f2(t) = ${formulas[0]}`)
-	formulaParser.evaluate(`f3(t) = ${formulas[1]}`)
-	const signal2 = new Signal()
-	const signal3 = new Signal()
-	signal2.emitSignal(100, 2);
-	signal3.emitSignal(100, 3);
-	signalsRef.current.push(signal2, signal3);
-	signalRef.current.combineSignals([signal2, signal3]);
-	signalRef.current.showVertex();
+	const renderChart = (signal: Observable<point>, index: number, chart: any) => {
+		subscriptionRef.current = signal.subscribe(point => {
+			chart.updateChart(point, index+1)
+		})
 	}
-	signalRef.current.appendOperators([take(3*samplesValue)]);
-}
 
-const handleSubmit = (event: any) => {
-	event.preventDefault();
-	if (!chartRef.current){ 
-		chartRef.current = createRealTimeLineChart(componentRef.current);
+	const appendOperators = () => {
+		if (mergeFunctions) {
+			formulaParser.evaluate(`f2(t) = ${formulas[0]}`)
+			formulaParser.evaluate(`f3(t) = ${formulas[1]}`)
+			const signal2 = new Signal()
+			const signal3 = new Signal()
+			signal2.emitSignal(100, 2);
+			signal3.emitSignal(100, 3);
+			signalsRef.current.push(signal2, signal3);
+			signalRef.current.combineSignals([signal2, signal3]);
+			signalRef.current.showVertex();
+		}
+		signalRef.current.appendOperators([take(3*samplesValue)]);
 	}
-	if (subscriptionRef.current) {
-		subscriptionRef.current.unsubscribe();
-	}
-	signalsRef.current = []
-	signalRef.current = new Signal();
-	signalsRef.current.push(signalRef.current)
-	chartRef.current.resetChart();
-	formulaParser.clear();
-	formulaParser.evaluate(`f1(t) = ` + formula);
-	signalRef.current.emitSignal(100, 1);
-	appendOperators();
-	console.log(formulas);
 
-	signalRef.current.changeInterval(intervalValue);
-	renderChart(signalRef.current.emittedSignal, 1, chartRef.current);
-	setIsPaused(() => false);
-};
+	const handleSubmit = (event: any) => {
+		event.preventDefault();
+		if (!chartRef.current) { 
+			chartRef.current = createRealTimeLineChart(componentRef.current);
+		}
+		if (subscriptionRef.current) {
+			subscriptionRef.current.unsubscribe();
+		}
+		signalsRef.current = []
+		signalRef.current = new Signal();
+		signalsRef.current.push(signalRef.current)
+		chartRef.current.resetChart();
+		formulaParser.clear();
+		formulaParser.evaluate(`f1(t) = ` + formula);
+		signalRef.current.emitSignal(100, 1);
+		appendOperators();
+		console.log(formulas);
 
-useEffect(() => {
-	if (signalRef.current){
-		if (isPaused) {
-			if (mergeFunctions) {
+		signalRef.current.changeInterval(intervalValue);
+		renderChart(signalRef.current.emittedSignal, 1, chartRef.current);
+		setIsPaused(() => false);
+	};
+
+	useEffect(() => {
+		if (signalRef.current){
+			if (isPaused) {
 				signalsRef.current.map(s => {
-				s.pause()
-				})
-			}
+					s.pause()
+					});
+				}
 			else {
-				signalRef.current.pause();
-			}
-		} else {
-			if (mergeFunctions) {
 				signalsRef.current.map(s => {
 					s.resume()
-			})
-			} else {
-				signalRef.current.resume();
+				});
 			}
 		}
-	}
-}, [isPaused]);
+	}, [isPaused]);
 
-useEffect(() => {
-	if (signalRef.current){
-	
-	}
-}, [filterValue]);
+	useEffect(() => {
+		if (signalRef.current){
+		
+		}
+	}, [filterValue]);
 
 
-useEffect(() => {
-	if (signalRef.current){
-		signalRef.current.changeInterval(intervalValue);
-	}
-}, [intervalValue]);
+	useEffect(() => {
+		if (signalRef.current){
+			signalsRef.current.map(s => {
+				s.changeInterval(intervalValue)
+				});
+		}
+	}, [intervalValue]);
 
-return (
+	return (
 	<div>
-	<form onSubmit={handleSubmit}>
-		<div className="label-input">
-			<label htmlFor="formula1">Formula 1:</label>
-			<input
-			type="text"
-			id="formula1"
-			value={formula}
-			onChange={handleFormula1Change}
-			/>
-		</div>
-
-		<div className="label-input">
-			<label htmlFor="interval">Interval:</label>
-			<input
-			type="range"
-			min="1"
-			max="100"
-			id="interval"
-			value={intervalValue}
-			onChange={handleIntervalChange}
-			/>
-		</div>
-		<div className="label-input">
-			<label htmlFor="filter">Filter:</label>
-			<input
-			type="range"
-			id="filter"
-			min="-10"
-			max="10" 
-			value={filterValue}
-			onChange={handleFilterChange}
-			/>
-		</div>
-		<div className="label-input">
-			<label htmlFor="samples">Samples:</label>
-			<input
-			type="numeric"
-			id="samples"
-			min="10"
-			max="5000"
-			value={samplesValue}
-			onChange={handleSamplesChange}
-			/>
-		</div>
-		<div>
-			<label>
-			<input
-				type="checkbox"
-				checked={mergeFunctions}
-				onChange={handleMergeFunctionsToggle}
-			/>
-			Merge Functions
-			</label>
-		</div>
-		{mergeFunctions &&
-		formulas.map((formula, index) => (
-			<div key={index} className="label-input">
-			<label htmlFor={`formula${index+1}`}>{`formula${index+1}`}</label>
-			<input
-				type="text"
-				id={`formula${index+1}`}
-				value={formula}
-				onChange={handleFormulaChange(index)}
-			/>
+		<form onSubmit={handleSubmit}>
+			<div className="label-input">
+				<label htmlFor="formula1">Formula 1:</label>
+				<input
+					type="text"
+					id="formula1"
+					value={formula}
+					onChange={handleFormula1Change}
+					/>
 			</div>
-		))}
+
+			<div className="label-input">
+				<label htmlFor="interval">Interval:</label>
+				<input
+				type="range"
+				min="1"
+				max="100"
+				id="interval"
+				value={intervalValue}
+				onChange={handleIntervalChange}
+				/>
+			</div>
+			<div className="label-input">
+				<label htmlFor="filter">Filter:</label>
+				<input
+				type="range"
+				id="filter"
+				min="-10"
+				max="10" 
+				value={filterValue}
+				onChange={handleFilterChange}
+				/>
+			</div>
+			<div className="label-input">
+				<label htmlFor="samples">Samples:</label>
+				<input
+				type="numeric"
+				id="samples"
+				min="10"
+				max="5000"
+				value={samplesValue}
+				onChange={handleSamplesChange}
+				/>
+			</div>
+			<div>
+				<label>
+				<input
+					type="checkbox"
+					checked={mergeFunctions}
+					onChange={handleMergeFunctionsToggle}
+				/>
+				Merge Functions
+				</label>
+			</div>
+			{mergeFunctions &&
+			formulas.map((formula, index) => (
+				<div key={index} className="label-input">
+				<label htmlFor={`formula${index+1}`}>{`formula${index+1}`}</label>
+				<input
+					type="text"
+					id={`formula${index+1}`}
+					value={formula}
+					onChange={handleFormulaChange(index)}
+				/>
+				</div>
+			))}
 		<button type="button" onClick={handlePauseToggle}>
 			{isPaused ? 'Resume' : 'Pause'}
 		</button>
--- src/signal.ts
+++ src/signal.ts
@@ -5,77 +5,77 @@ import { filter } from "rxjs";
 
 export class Signal {
 
-  private intervalSubject = new BehaviorSubject<number>(1);
-  private pauseSubject = new BehaviorSubject<boolean>(false);
-  private filterValue = new BehaviorSubject<number>(Infinity);
-  private counter = 0;
-  emittedSignal: Observable<point>;
+	private intervalSubject = new BehaviorSubject<number>(1);
+	private pauseSubject = new BehaviorSubject<boolean>(false);
+	private filterValue = new BehaviorSubject<number>(Infinity);
+	private counter = 0;
+	emittedSignal: Observable<point>;
 
-  pausableObservable = this.intervalSubject.pipe(
-    switchMap(intervalValue => this.pauseSubject.pipe(
-      switchMap(paused => paused ? NEVER : interval(intervalValue)
-        .pipe(
-          startWith(this.counter), 
-          switchMap(() => {
-            this.counter++;
-            return of(this.counter);
-          })
-        )
-      )
-    )),
-  );
+	pausableObservable = this.intervalSubject.pipe(
+		switchMap(intervalValue => this.pauseSubject.pipe(
+			switchMap(paused => paused ? NEVER : interval(intervalValue)
+				.pipe(
+				startWith(this.counter), 
+				switchMap(() => {
+					this.counter++;
+					return of(this.counter);
+				})
+				)
+			)
+        )),
+	);
 
-  delete(){
-    delete this.emittedSignal;
-  }
+	combineSignals(signals: Signal[]) {
+		const emittedSignals = signals.flatMap(s => s.emittedSignal);
+		emittedSignals.push(this.emittedSignal);
+		this.emittedSignal = combineLatest(emittedSignals, (...points: point[]) => {
+		return points.reduce((acc, value) => {
+			return {
+			y: acc.y * value.y,
+			x: value.x
+			}
+		});
+		})
+	}
 
-  changeInterval(value: number) {
-    this.intervalSubject.next(value);
-  }
+	showVertex(){
+		this.appendOperators([
+		bufferCount(3, 1),
+		map((t) => ({
+			x: t[1].x,
+			y: t[1].y,
+			isVertex: ((t[0].y < t[1].y && t[2].y < t[1].y) || (t[0].y > t[1].y && t[2].y > t[1].y))
+		    }))
+		]
+	)}
 
-  pause() {
-    this.pauseSubject.next(true);
-  }
+	appendOperators(operators: OperatorFunction<any, any>[]) {
+		this.emittedSignal = operators.reduce((observable, operator) => observable.pipe(operator), this.emittedSignal);
+	}
 
-  resume() {
-    this.pauseSubject.next(false);
-  }
+	emitSignal(fraction: number, formulaIndex: number) {
+		this.emittedSignal = this.pausableObservable.pipe(
+		map((t) => ({
+			x: t,
+			y: formulaParser.evaluate(`f${formulaIndex}(${t / fraction})`),
+			})),
+		)
+	}
 
-  combineSignals(signals: Signal[]) {
-    const emittedSignals = signals.flatMap(s => s.emittedSignal);
-    emittedSignals.push(this.emittedSignal);
-    this.emittedSignal = combineLatest(emittedSignals, (...points: point[]) => {
-      return points.reduce((acc, value) => {
-        return {
-          y: acc.y * value.y,
-          x: value.x
-        }
-      });
-    })
-  }
+    delete() {
+	    delete this.emittedSignal;
+	}
 
-  showVertex(){
-    this.appendOperators([
-      bufferCount(3, 1),
-      map((t) => ({
-        x: t[1].x,
-        y: t[1].y,
-        isVertex: ((t[0].y < t[1].y && t[2].y < t[1].y) || (t[0].y > t[1].y && t[2].y > t[1].y))
-      }))
-    ]
-  )}
+	changeInterval(value: number) {
+        this.intervalSubject.next(value);
+	}
 
-  appendOperators(operators: OperatorFunction<any, any>[]) {
-    this.emittedSignal = operators.reduce((observable, operator) => observable.pipe(operator), this.emittedSignal);
-  }
+	pause() {
+		this.pauseSubject.next(true);
+	}
 
-  emitSignal(fraction: number, formulaIndex: number) {
-    this.emittedSignal = this.pausableObservable.pipe(
-      map((t) => ({
-        x: t,
-        y: formulaParser.evaluate(`f${formulaIndex}(${t / fraction})`),
-      })),
-    )
-  }
+	resume() {
+		this.pauseSubject.next(false);
+	}
 }
 
